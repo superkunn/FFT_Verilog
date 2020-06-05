80466dc1d70ef47f982fe2c4263272470e0aee8c
superkun <superkunn@users.noreply.github.com>
2020-06-06 07:07:02 +0800

    Update mul_complex.v

--- mul_complex.v
+++ mul_complex.v
@@ -1,16 +1,19 @@
 //计算两个复数的乘积
-module mul_complex(xr, xi, yr, yi, zr, zi);
+module mul_complex(a, b, c, d, x, y);
 
-input signed [32 - 1:0] xr, xi, yr, yi;
+input signed [32 - 1:0] a, b, c, d;
 
-output signed[32 - 1:0] zr, zi;
+output signed[32 - 1:0] x, y;
 
-wire signed [32 * 2 - 1:0] wr, wi;
+wire signed [32 * 2 - 1:0] ac, bd, xx, yy;
+//(a + bi) * (c + di) = ac - bd + (bc + ad)i
+//bc + ad = (a + b)(c + d) - ac - bd
+assign ac = a * c;
+assign bd = b * d;
+assign xx = ac - bd;
+assign yy = (a + b) * (c + d) - ac - bd;
 
-assign wr = xr * yr - xi * yi;
-assign wi = xr * yi + xi * yr;
-
-assign zr = wr[32 + 15:16];
-assign zi = wi[32 + 15:16];
+assign x = xx[32 + 15:16];
+assign y = yy[32 + 15:16];
 
 endmodule
